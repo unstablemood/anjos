815230803d72880d8621b522344851c16a314b7b
IsaIsa <55638438+unstablemood@users.noreply.github.com>
2022-08-28 00:50:23 -0300

    Adicionei scrolls
    Adicionei scrolls nos botões

--- anjostyle.js
+++ anjostyle.js
@@ -4,11 +4,47 @@ function irCinza(){
     fui.scrollIntoView();
 }
 
-function irCinza(){
-    const fui = document.getElementById('cinza');
-    fui.scrollIntoView();
+function voltarPrimeira(){
+    const volto = document.getElementById('somUm');
+    volto.scrollIntoView();
 }
 
+function irSemchuva() {
+    const irsem = document.getElementById('quadroCavalos');
+    irsem.scrollIntoView();
+}
+
+var x = document.getElementById("chuva");
+     function playChuva() { 
+    x.play(); 
+  } 
+
+  function pauseChuva() { 
+    x.pause(); 
+    }
+
+    function imagemSim(){
+        const parafinal = document.getElementById('finalSim');
+        parafinal.scrollIntoView();
+    }
+
+    function imagemNao(){
+        const parafinalNao = document.getElementById('finalNao');
+        parafinalNao.scrollIntoView();
+    }
+
+    function irBlack(){
+        const noescuro = document.getElementById('Black');
+        noescuro.scrollIntoView();
+    }
+    function irLuz(){
+        const naluz = document.getElementById('luz');
+        naluz.scrollIntoView();
+    }
+
+    function chuvaLonga() {
+
+    }
 
 const chuva = document.getElementById('quadro1');
 const frameQuadrado =  new Image();
